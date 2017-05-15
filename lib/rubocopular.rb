00512409027f1d2ed440ab1820aec8ba9614322e
Jônatas Davi Paganini <jonatas.paganini@toptal.com>
2017-05-15 15:39:17 -0300

    Add file and line number to printable results

--- lib/rubocopular.rb
+++ lib/rubocopular.rb
@@ -7,8 +7,12 @@ module Rubocopular
     RuboCop::ProcessedSource.new(code.to_s, 2.3).ast
   end
 
+  def self.parse_source(path)
+    RuboCop::ProcessedSource.new(IO.read(path), 2.3, path)
+  end
+
   def self.test(pattern, code)
-    code = node(code) if code.is_a?(String)
+    code = node(code) unless code.is_a?(RuboCop::AST::Node)
     RuboCop::NodePattern.new(pattern).match(code)
   end
 
